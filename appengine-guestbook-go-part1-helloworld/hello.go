f76937d107ec71d5d8015c03f3268d8a529065c2
vamsi199 <vamsi.1991992@gmail.com>
2017-07-01 14:02:16 -0400

    Hello world running code

--- appengine-guestbook-go-part1-helloworld/hello.go
+++ appengine-guestbook-go-part1-helloworld/hello.go
@@ -1,14 +1,25 @@
-package hello
+package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func init() {
-    http.HandleFunc("/", handler)
+func main() {
+	http.HandleFunc("/", handle)
+	http.HandleFunc("/_ah/health", healthCheckHandler)
+	log.Print("Listening on port :8080")
+	log.Fatal(http.ListenAndServe(":8080",nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+func handle(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path !="/"{
+		http.NotFound(w,r)
+		return
+	}
+	fmt.Fprint(w, "Hello, world!")
+}
+func healthCheckHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w,"ok")
 }
