f67996da8fe7e82faf038d63fc08688b2ee126e0
Vamsi G <vamsig@Vamsis-MacBook-Pro.local>
2017-12-15 11:20:25 -0700

    added grpc fmt

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -1,41 +1,40 @@
 package main
 
 import (
+	"fmt"
+	pb "github.com/vamsi199/examples/grpc/pb"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	pb "github.com/vamsi199/examples/grpc/pb"
-	"log"
-	"fmt"
 	"io"
-
+	"log"
 )
 
-func main () {
+func main() {
 	//var i pb.Input
-	i := &pb.Input{"hello","vamsi"}
-	i1 := &pb.Input1{1,"karthik"}
+	i := &pb.Input{"hello", "vamsi"}
+	i1 := &pb.Input1{1, "karthik"}
 	i2 := &pb.Input2{"stream"}
-	conn,err:=grpc.Dial("localhost:8080",grpc.WithInsecure())
-	if err!=nil {
-		log.Fatalln("error dailing",err)
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalln("error dailing", err)
 	}
 
-	client:=pb.NewHelloClient(conn)
-	resp,err:=client.Sayhello(context.Background(),i)
+	client := pb.NewHelloClient(conn)
+	resp, err := client.Sayhello(context.Background(), i)
 	if err != nil {
-		log.Fatalln("cannot get the response",err)
+		log.Fatalln("cannot get the response", err)
 	}
 	fmt.Println(resp.Name)
 
-	resp,err=client.SayHelloAll(context.Background(),i1)
-	if err!=nil {
-		log.Fatalln("error dailing",err)
+	resp, err = client.SayHelloAll(context.Background(), i1)
+	if err != nil {
+		log.Fatalln("error dailing", err)
 	}
 	fmt.Println(resp)
 
-	stream,err:=client.StreamAll(context.Background(),i2)
+	stream, err := client.StreamAll(context.Background(), i2)
 	if err != nil {
-		log.Fatalln("cannot get the response stream",err)
+		log.Fatalln("cannot get the response stream", err)
 
 	}
 	for {
--- grpc/server/main.go
+++ grpc/server/main.go
@@ -1,66 +1,61 @@
 package main
 
 import (
+	"fmt"
+	pb "github.com/vamsi199/examples/grpc/pb"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	pb "github.com/vamsi199/examples/grpc/pb"
-	"net"
 	"log"
-	"fmt"
+	"net"
 )
-type content struct {
 
-	 s string
+type content struct {
+	s string
 }
 type val struct {
 	s string
 }
 
-func main () {
-	lis,err:=net.Listen("tcp","localhost:8080")
+func main() {
+	lis, err := net.Listen("tcp", "localhost:8080")
 	if err != nil {
-		log.Fatal("cannot listen to the post 8080",err)
+		log.Fatal("cannot listen to the post 8080", err)
 	}
 
-    server:=grpc.NewServer()
-    fmt.Print(lis)
-    pb.RegisterHelloServer(server,content{})
+	server := grpc.NewServer()
+	fmt.Print(lis)
+	pb.RegisterHelloServer(server, content{})
 	fmt.Print("2")
-    server.Serve(lis)
+	server.Serve(lis)
 }
 
-func (content) Sayhello (ctx context.Context, i *pb.Input) (*pb.Output,error) {
+func (content) Sayhello(ctx context.Context, i *pb.Input) (*pb.Output, error) {
 
+	fmt.Println(i.Name, i.Wish)
+	o := pb.Output{i.Name}
 
-	fmt.Println(i.Name,i.Wish)
-	o:= pb.Output{i.Name}
-
-	return &o,nil
-
-
+	return &o, nil
 
 }
 
-func (content) SayHelloAll(ctx context.Context, i *pb.Input1) (o *pb.Output,err error) {
-	o=&pb.Output{i.Alias}
+func (content) SayHelloAll(ctx context.Context, i *pb.Input1) (o *pb.Output, err error) {
+	o = &pb.Output{i.Alias}
 
-
-return o,nil
+	return o, nil
 }
 
-func (content) StreamAll(i2 *pb.Input2,stream pb.Hello_StreamAllServer)  (err error) {
+func (content) StreamAll(i2 *pb.Input2, stream pb.Hello_StreamAllServer) (err error) {
 	s := content{"stream1"}
 	s1 := content{"stream2"}
-     for i:=0;i<2;i++ {
-     	if i==0 {
+	for i := 0; i < 2; i++ {
+		if i == 0 {
 
 			err = stream.Send(s)
 		}
 		err = stream.Send(s1)
 		return err
-	 }
+	}
 
-	 return nil
-
+	return nil
 
 }
